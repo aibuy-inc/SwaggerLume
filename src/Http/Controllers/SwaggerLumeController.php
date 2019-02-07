43f52b3316c183b52095e9fd59512033ff9abf69
cinsaydev <rscott@cinsay.com>
2019-02-07 15:32:26 -0600

    Update SwaggerLumeController.php

--- src/Http/Controllers/SwaggerLumeController.php
+++ src/Http/Controllers/SwaggerLumeController.php
@@ -46,7 +46,7 @@ class SwaggerLumeController extends BaseController
         $response = new Response(
             view('swagger-lume::index', [
                 'secure' => Request::secure(),
-                'urlToDocs' => route('swagger-lume.docs'),
+                'urlToDocs' => route('swagger-lume.docs', env('URL_SCHEMA_SECURE', false)),
                 'operationsSorter' => config('swagger-lume.operations_sort'),
                 'configUrl' => config('swagger-lume.additional_config_url'),
                 'validatorUrl' => config('swagger-lume.validator_url'),
